083365e67cce1802498ed8bcc656f2236a371760
Julio Caro <jcarocota@gmail.com>
2025-05-15 21:10:09 -0700

    Agregamos el Gauge al mainactivity.xml

--- app/src/main/java/com/ebc/customanimation/MainActivity.kt
+++ app/src/main/java/com/ebc/customanimation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ebc.customanimation
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +14,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.ebc.customanimation.ui.theme.CustomAnimationTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var motionLayout: MotionLayout
+    private lateinit var gaugeView: CustomGaugeView
+    private lateinit var infoText: TextView
+    private lateinit var btnReset: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CustomAnimationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        setContentView(R.layout.mainactivity)
+
+        gaugeView = findViewById(R.id.gaugeView)
+
+        gaugeView.setOnClickListener {
+            val randomAngle = (0..180).random().toFloat()
+            gaugeView.animateTo(randomAngle)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CustomAnimationTheme {
-        Greeting("Android")
     }
 }
